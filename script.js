ccb9c2d4d308bf5ce54f88232d4fed00ca9d651f
Amy Schremp <amyschremp@Amys-MacBook-Pro.local>
2020-05-19 14:03:02 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const songs = [
+    "bensound-anewbeginning.mp3",
+    "bensound-creativeminds.mp3",
+    "bensound-memories.mp3",
+    "bensound-summer.mp3",
+    "bensound-ukulele.mp3"
+]
+
+const createSongList = () => {
+    const list = document.createElement('ol')
+
+    for (let i = 0; i < songs.length; i++) {
+        const item = document.createElement('li')
+        item.appendChild(document.createTextNode(songs[i]))
+
+        list.appendChild(item)
+    } 
+
+    return list
+}
+
+document.getElementById('songlist').appendChild(createSongList())
+
+songlist.onclick = (e) => {
+    const clickedItem = e.target
+    const source = document.getElementById('source')
+    source.src = 'songs/' + clickedItem.innerText
+
+    document.getElementById('currentlyPlayingSong').innerText = "Now Playing: "
+    document.getElementById('currentSong').innerText = clickedItem.innerText
+
+    player.load()
+    player.play()
+}
+
+const playAudio = () => {
+    if(player.readyState) {
+        player.play()
+    }
+}
+
+const pauseAudio = () => {
+    player.pause()
+}
+
+const slider = document.getElementById('volumeSlider')
+    slider.oninput = (e) => {
+    const volume = e.target.value
+    player.volume = volume
+}
+
+const updateProgress = () => {
+    if (player.currentTime > 0) {
+        const progressBar = document.getElementById('progress')
+
+        progressBar.value = (player.currentTime / player.duration) * 100
+    }
+}
